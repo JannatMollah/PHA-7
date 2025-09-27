f4671dcfac72a715b175167364f0b5684ee04f5b
JannatMollah <jannatmollah@gmail.com>
2025-09-27 21:09:01 +0600

    ticket card update

--- src/components/TicketsCard/TicketsCard.jsx
+++ src/components/TicketsCard/TicketsCard.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const TicketsCard = ({ticket}) => {
     return (
-        <div className="card w-100 bg-base-100 card-sm shadow-sm">
+        <div className="card w-100 bg-base-100 card-sm shadow-sm cursor-pointer" >
             <div className="card-body">
                 <div className="flex items-center justify-between">
                     <h2 className="card-title">{ticket.title}</h2>
-                    <div className="badge bg-[#B9F8CF] text-[#0B5E06]">{ticket.status}</div>
+                    <div className={`badge ${ticket.status === "Open" ? "bg-[#B9F8CF] text-[#0B5E06]" : "bg-[#F8F3B9] text-[#9C7700]"}`}>{ticket.status}</div>
                 </div>
                 <p className="text-sm text-[#627382]">{ticket.description}</p>
                 <div className="flex items-center justify-between">
                     <div className="flex items-center justify-center gap-2">
                         <p className="text-sm text-[#627382]">#{ticket.id}</p>
-                        <p className="font-medium text-sm text-[#F83044]">{ticket.priority}</p>
+                        <p className={`font-medium text-sm ${ticket.priority === "HIGH PRIORITY" ? "text-[#F83044]" : ticket.priority === "MEDIUM PRIORITY" ? "text-[#FEBB0C]" : "text-[#02A53B]"}`}>{ticket.priority}</p>
                     </div>
                     <div className="flex items-center justify-center text-sm text-[#627382] gap-2">
                         <p>{ticket.customer}</p>
