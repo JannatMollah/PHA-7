ac7c31cd8f91e82948d484f8136abd887d9af9b5
JannatMollah <jannatmollah@gmail.com>
2025-09-27 14:06:59 +0600

    status card

--- src/components/StatusCard/StatusCard.jsx
+++ src/components/StatusCard/StatusCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import cardr from '../../assets/vector1.png'
 
 const StatusCard = () => {
     return (
         <div className="max-w-[1200px] mt-10 mx-auto flex items-center justify-between">
-            <div className="bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-lg w-[590px] h-[200px] flex flex-col items-center justify-center">
+
+            <div className="relative text-white rounded-lg w-[590px] h-[200px] flex flex-col items-center justify-center bg-gradient-to-r from-[#632EE3] to-[#9F62F2]">
+                <div className="absolute inset-0 bg-[url('/vector1.png')] bg-no-repeat transform -scale-x-100"></div>
+                <div className="absolute inset-0 bg-[url('/vector1.png')] bg-no-repeat"></div>
                 <p>In-Progress</p>
                 <span className="font-semibold text-4xl">0</span>
             </div>
-            <div className="bg-linear-to-r from-[#54CF68] to-[#00827A] text-white rounded-lg w-[590px] h-[200px] flex flex-col items-center justify-center">
+
+            <div className=" relative bg-linear-to-r from-[#54CF68] to-[#00827A] text-white rounded-lg w-[590px] h-[200px] flex flex-col items-center justify-center">
+                <div className="absolute inset-0 bg-[url('/vector1.png')] bg-no-repeat transform -scale-x-100"></div>
+                <div className="absolute inset-0 bg-[url('/vector1.png')] bg-no-repeat"></div>
                 <p>Resolved</p>
                 <span className="font-semibold text-4xl">0</span>
             </div>
